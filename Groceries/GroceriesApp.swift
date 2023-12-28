2580367aaed1acd946017d8350a9055216ab2804
Kane Sweet <kanesweet11@gmail.com>
2023-12-28 15:28:14 -0600

    Initial Commit

---
+++ Groceries/GroceriesApp.swift
@@ -0,0 +1,20 @@
+//
+//  GroceriesApp.swift
+//  Groceries
+//
+//  Created by Kane Sweet on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct GroceriesApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
